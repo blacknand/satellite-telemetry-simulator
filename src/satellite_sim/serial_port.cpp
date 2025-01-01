88fef72f90b2d41fde4acfc9632a1155bfcc4cb0
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-01-01 21:46:52 +0000

    restart serial port

--- src/satellite_sim/serial_port.cpp
+++ src/satellite_sim/serial_port.cpp
@@ -2,58 +2,45 @@
 
 #include <QSerialPortInfo>
 #include <QSerialPort>
+#include <QLabel>
+#include <QMessageBox>
+#include <QTimer>
+
+#include <chrono>
+
+static constexpr std::chrono::seconds kWriteTimeout = std::chrono::seconds{5};
 
 
 SerialPort::SerialPort(QWidget *parent) : 
-    QWidget(parent),
-    m_ui(new Ui::SerialPort),
-    m_serial(new QSerialPort(this))
+    QMainWindow(parent),
+    sp_ui(new SerialPort::SerialPort),
+    sp_status(new QLabel),
+    sp_timer(new QTimer),
+    sp_serial(new QSerialPort(this)),
 {
-    serial = new QSerialPort(this);
-    connect(serial, &QSerialPort::readyRead, this, &SerialPort::readData);
+    connect(sp_serial, &QSerialPort::errorOccured, this, &SerialPort::handleError);
+    connect(sp_serial, &QTimer::timeout, this, &SerialPort::handleWriteTimeout);
+    sp_timer->setSingleShot(true);
+    connect(sp_serial, &QSerialPort::readyRead, this, &SerialPort::readData);
+    connect(m_console, &Console::getData, this, &SerialPort::writeData);
 }
 
 
-void SerialPort::OpenSerialPort()
+SerialPort::~SerialPort()
+// Not sure if needed but keep for now
 {
-    const SettingsDialog::Settings p = m_settings->settings();
-    m_serial->setPortName(p.name);
-    m_serial->setBaudRate(p.baudRate);
-    m_serial->setDataBits(p.dataBits);
-    m_serial->setParity(p.parity);
-    m_serial->setStopBits(p.stopBits);
-    m_serial->setFlowControl(p.flowControl);
-    if (m_serial->open(QIODevice::ReadWrite)) {
-        m_console->setEnabled(true);
-        m_console->setLocalEchoEnabled(p.localEchoEnabled);
-        m_ui->actionConnect->setEnabled(false);
-        m_ui->actionDisconnect->setEnabled(true);
-        m_ui->actionConfigure->setEnabled(false);
-        showStatusMessage(tr("Connected to %1 : %2, %3, %4, %5, %6")
-                          .arg(p.name, p.stringBaudRate, p.stringDataBits,
-                               p.stringParity, p.stringStopBits, p.stringFlowControl));
-    } else {
-        QMessageBox::critical(this, tr("Error"), m_serial->errorString());
-
-        showStatusMessage(tr("Open error"));
-    }
+    delete sp_ui;
 }
 
 
-void SerialPort::CloseSerialPort()
+void SerialPort::OpenSerialPort() 
 {
-    if (m_serial->isOpen())
-        m_serial->close();
-    m_console->setEnabled(false);
-    m_ui->actionConnect->setEnabled(true);
-    m_ui->actionDisconnect->setEnabled(false);
-    m_ui->actionConfigure->setEnabled(true);
-    showStatusMessage(tr("Disconnected"));
-}
+    if (sp_serial->isOpen()) { closeSerialPort(); }
 
-
-void SerialPort::ReadData(void)
-{
-    QByteArray data = m_serial->readAll();
-    m_console->putData(data);
+    sp_serial->setPortName("/dev/tty.usbmodem1101");
+    // sp_serial->setBaudRate(115200);
+    // sp_serial->setDataBits(p.dataBits);
+    // sp_serial->setParity(p.parity);
+    // sp_serial->setStopBits(p.stopBits);
+    // sp_serial->setFlowControl(p.flowControl);
 }
--- src/satellite_sim/serial_port.h
+++ src/satellite_sim/serial_port.h
@@ -1,14 +1,31 @@
 #ifndef SERIAL_PORT_H
 #define SERIAL_PORT_H
 
-class SerialPort {
-public:
-    SerialPort(QWidget *parent = nullptr);
-    OpenSerialPort();
-    CloseSerialPort();
-    ReadData();
-private:
+#include <QMainWindow>
+#include <QSerialPort>
 
+class QLabel;
+class QTimer;
+
+class SerialPort : public QMainWindow 
+{
+    Q_OBJECT
+public:
+    explicit SerialPort(QWidget *parent = nullptr);
+    ~SerialPort();      // Probably do not need this since serial port will never be closed
+
+private slots:
+    void openSerialPort();
+    void closeSerialPort();
+    void readData();
+    void handleError(QSerialPort::SerialPortError error);
+private:
+    void showStatusMessage(const QString &message);
+
+    SerialPort::SerialPort *sp_ui = nullptr;
+    QLabel *sp_status = nullptr;
+    QTimer *sp_timer = nullptr;
+    QSerialPort *sp_serial = nullptr;
 };
 
 #endif // SERIAL_PORT_H
