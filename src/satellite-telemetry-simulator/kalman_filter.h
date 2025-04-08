9cb4342379452c4dcb726019629d65edc84833a9
Nathan Blackburn <nblackburndeveloper@icloud.com>
2025-04-08 10:46:30 +0100

    add kalman filter h

---
+++ src/satellite-telemetry-simulator/kalman_filter.h
@@ -0,0 +1,35 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef KALMAN_FILTER_H
+#define KALMAN_FILTER_H
+
+#pragma once
+
+
+typedef struct {
+    float state[6]; // [px, py, pz, vx, vy, vz]
+    float P[6][6];  // Covariance matrix
+} KalmanState;
+
+
+typedef struct {
+    float F[6][6];  // State transition
+    float B[6][3];  // Control matrix
+    float H[3][6];  // Measurement matrix
+    float Q[6][6];  // Process noise
+    float R[3][3];  // Measurement noise
+} KalmanFilter;
+
+
+void init_kalman(KalmanState* state, KalmanFilter* kf, float dt);
+void kalman_predict(KalmanState* state, KalmanFilter* kf, float accel[3]);
+void kalman_update(KalmanState* state, KalmanFilter* kf, float measured_v[3]);
+
+
+#endif  // KALMAN_FILTER_H
+
+#ifdef __cplusplus
+}
+#endif
